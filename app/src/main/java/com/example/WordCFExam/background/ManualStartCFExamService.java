e62fbdd9a4425a0bf3c5be30371c7534a57efb06
p3tzata <p3tzata_@abv.bg>
2021-07-05 12:17:32 +0300

    fix hang up; autofocus on Word proceed exam

--- app/src/main/java/com/example/WordCFExam/background/ManualStartCFExamService.java
+++ app/src/main/java/com/example/WordCFExam/background/ManualStartCFExamService.java
@@ -3,6 +3,8 @@ package com.example.WordCFExam.background;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.os.IBinder;
 
 import com.example.WordCFExam.activity.MainActivity;
@@ -23,6 +25,8 @@ public class ManualStartCFExamService extends Service
             //alarm.sendNotification_works(this);
             alarm.sendNotification(this,100, MainActivity.class,null,"Schedule CFExam started manual","Started");
             String debug="";
+
+           
             return START_STICKY;
         }
 
